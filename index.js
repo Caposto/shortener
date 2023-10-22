267b5f0fc76b1c454a7e810c8b1bfe1c6aaa7535
Caposto <christiand.apostol@outlook.com>
2023-10-22 12:33:42 -0400

    added yaml npm package

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-// Adding HTML meta tag to the head
-
+// Method for redirect: Adding HTML meta tag to the head
 
 // 1. Load all the urls from redirects.yml
-
+const YAML = require('yaml')
 
 
 // 2. Generate an html page for each redirect url from template.html
