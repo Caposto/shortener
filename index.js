bbe1a11695f622dc8443bc26c4187206b2866635
Caposto <christiand.apostol@outlook.com>
2023-10-22 12:46:03 -0400

    looped over redirects

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ const fs = require('fs'); // file system
 const path = require('path') // windows -> linux file system
 
 const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8');
-console.log(redirectsFile);
+const redirects = YAML.parse(redirectsFile);
+
+console.log(redirects);
 
 // 2. Generate an html page for each redirect url from template.html
+
+// load html template
+const tempalteHTML = fs.readFileSync(path.join(__dirname, 'template.html'), 'utf-8');
+
+// loop through all url redirects, and generate an html page
+for (let [slug, url] of Object.entries(redirects)) {
+  console.log('Generating HTML Page for ', slug)
+}
