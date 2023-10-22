fe067c37e330557b0ef9ba5752692d5262be6007
Caposto <christiand.apostol@outlook.com>
2023-10-22 12:37:26 -0400

    reading from redirects file

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Method for redirect: Adding HTML meta tag to the head
 
 // 1. Load all the urls from redirects.yml
-const YAML = require('yaml')
+const YAML = require('yaml');
+const fs = require('fs'); // file system 
+const path = require('path') // windows -> linux file system
 
+const redirectsFile = fs.readFileSync(path.join(__dirname, 'redirects.yml'), 'utf-8');
+console.log(redirectsFile);
 
 // 2. Generate an html page for each redirect url from template.html
